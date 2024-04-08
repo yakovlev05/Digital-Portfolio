2ec850c298cd68b14864eda349cf7fe71427949d
Alexey Yakovlev <alexey.yakovlev.05@mail.ru>
2024-04-08 15:58:17 +0500

    add IPasswordService.cs

---
+++ Server/Services/Interfaces/IPasswordService.cs
@@ -0,0 +1,8 @@
+﻿namespace Server.Services.Interfaces;
+
+public interface IPasswordService
+{
+    public string HashPassword(string password);
+    public bool VerifyPassword(string password, string hash);
+    public string GeneratePassword();
+}
