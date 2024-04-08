739644487ab8231940974062e7ab3976c154ebcd
Alexey Yakovlev <alexey.yakovlev.05@mail.ru>
2024-04-08 14:58:14 +0500

    add Authentication and Authorization

---
+++ Server/Program/AuthSettings.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Server.Program;
+
+public class AuthSettings
+{
+    public static void AddAuthentication(WebApplicationBuilder builder)
+    {
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = AuthenticationOptions.Issuer,
+                    ValidateAudience = true,
+                    ValidAudience = AuthenticationOptions.Audience,
+                    ValidateLifetime = true,
+                    IssuerSigningKey = AuthenticationOptions.GetSymmetricSecurityKey(),
+                    ValidateIssuerSigningKey = true,
+                    ClockSkew = TimeSpan.Zero // Истечение токена строго по времени
+                };
+            });
+    }
+}
+
+public static class AuthenticationOptions
+{
+    public static readonly string? Issuer = Program.Config["Jwt:Issuer"]?.ToString();
+    public static readonly string? Audience = Program.Config["Jwt:Audience"]?.ToString();
+    private static readonly string? Key = Program.Config["Jwt:Key"]?.ToString();
+
+    public static SymmetricSecurityKey GetSymmetricSecurityKey() =>
+        new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(Key ?? throw new InvalidOperationException("Jwt:Key is not set in configuration.")));
+}
+
+public static class AuthenticationExtensions
+{
+    public static void AddAuthenticationAndAuthorization(this WebApplicationBuilder builder)
+    {
+        AuthSettings.AddAuthentication(builder);
+        builder.Services.AddAuthorization();
+    }
+
+    public static void UseAuthenticationAndAuthorization(this WebApplication app)
+    {
+        app.UseAuthentication();
+        app.UseAuthorization();
+    }
+}
--- Server/Program/Program.cs
+++ Server/Program/Program.cs
@@ -1,18 +1,22 @@
+using System.Collections;
+
 namespace Server.Program;
 
 public static class Program
 {
+    public static readonly IDictionary Config = Environment.GetEnvironmentVariables();
+
     private static void Main()
     {
         var builder = WebApplication.CreateBuilder();
 
-
+        builder.AddAuthenticationAndAuthorization();
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
         var app = builder.Build();
 
-
+        app.UseAuthenticationAndAuthorization();
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
