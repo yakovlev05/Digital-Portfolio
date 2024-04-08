771165e75224a147abaf8e82a3e7dbb7403ce554
Alexey Yakovlev <alexey.yakovlev.05@mail.ru>
2024-04-08 16:45:54 +0500

    update UserEntity.cs

--- Server/DataBase/Entities/UserEntity.cs
+++ Server/DataBase/Entities/UserEntity.cs
@@ -4,7 +4,7 @@ public class UserEntity
 {
     public int Id { get; init; }
     public string Login { get; set; } = null!;
-    public string Password { get; set; } = null!;
+    public string HashedPassword { get; set; } = null!;
     public string Email { get; set; } = null!;
     public string Name { get; set; } = null!;
     public string SecondName { get; set; } = null!;
