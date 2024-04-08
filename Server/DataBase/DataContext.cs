555db729a5df309870f8f9da6e4e7bf096015a63
Alexey Yakovlev <alexey.yakovlev.05@mail.ru>
2024-04-08 15:54:08 +0500

    add DataContext.cs

---
+++ Server/DataBase/DataContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Server.DataBase.Entities;
+
+namespace Server.DataBase;
+
+public class DataContext : DbContext
+{
+    public DbSet<UserEntity> Users { get; set; }
+    public DbSet<FileEntity> Files { get; set; }
+
+    public DataContext()
+    {
+        Database.Migrate();
+    }
+
+    public new async Task<int> SaveChanges()
+    {
+        return await base.SaveChangesAsync();
+    }
+
+    public DbSet<T> DbSet<T>() where T : class
+    {
+        return Set<T>();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(DatabaseSettings.CreateConnectionString());
+    }
+}
