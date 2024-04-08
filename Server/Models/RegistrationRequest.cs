96f1a328da56f3e5abf7cb3bc6fec9095175df4f
Alexey Yakovlev <alexey.yakovlev.05@mail.ru>
2024-04-08 17:56:37 +0500

    add RegistrationRequest.cs

---
+++ Server/Models/RegistrationRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Server.Models;
+
+public record RegistrationRequest(
+    string Login,
+    string Email,
+    string SecondName,
+    string Name,
+    string Password,
+    string ConfirmPassword
+);
