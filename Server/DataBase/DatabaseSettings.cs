689bdecb74fce861e272a1926bee737fea2933b8
Alexey Yakovlev <alexey.yakovlev.05@mail.ru>
2024-04-08 15:53:52 +0500

    add DatabaseSettings.cs

---
+++ Server/DataBase/DatabaseSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace Server.DataBase;
+
+public static class DatabaseSettings
+{
+    private static readonly string Host = "db";
+    private static readonly string Port = "5432";
+    private static readonly string? Username = Program.Program.Config["POSTGRES_USER"]?.ToString();
+    private static readonly string? Password = Program.Program.Config["POSTGRES_PASSWORD"]?.ToString();
+    private static readonly string? Database = Program.Program.Config["POSTGRES_DB"]?.ToString();
+
+    public static string CreateConnectionString() =>
+        $"Host={Host};Port={Port};Username={Username};Password={Password};Database={Database};";
+}
