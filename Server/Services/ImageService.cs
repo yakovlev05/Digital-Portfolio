b33924bc8dca72fb58152fe05cc97e00d4f96bf7
Alexey Yakovlev <alexey.yakovlev.05@mail.ru>
2024-04-19 19:24:18 +0500

    add ImageService

---
+++ Server/Services/ImageService.cs
@@ -0,0 +1,29 @@
+﻿using Server.Services.Interfaces;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Processing.Processors.Transforms;
+
+namespace Server.Services;
+
+public class ImageService : IImageService
+{
+    public async Task<MemoryStream> ResizeImage(Stream image, int width, int height)
+    {
+        using var resizedImage = await Image.LoadAsync(image);
+        resizedImage.Mutate(x => x.Resize(new ResizeOptions
+        {
+            Size = new Size(width, height),
+            Mode = ResizeMode.Max
+        }));
+        var memoryStream = new MemoryStream();
+        await resizedImage.SaveAsJpegAsync(memoryStream);
+        memoryStream.Position = 0;
+        return memoryStream;
+    }
+
+    public Task<FileStream> CompressImage(FileStream image)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Server/Services/Interfaces/IImageService.cs
@@ -0,0 +1,7 @@
+﻿namespace Server.Services.Interfaces;
+
+public interface IImageService
+{
+    public Task<MemoryStream> ResizeImage(Stream image, int width, int height);
+    public Task<FileStream> CompressImage(FileStream image);
+}
