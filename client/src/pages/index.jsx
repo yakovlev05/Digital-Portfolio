c75cb84163023cb8634a20397dc87816d33e93e8
yakovlev05 <alexey.yakovlev.05@mail.ru>
2024-04-29 13:03:49 +0500

    test page

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,5 +1,6 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import App from "../App";
+import IndexPage from "./index"
 
 const AppRouter = () => {
     return (
@@ -7,6 +8,7 @@ const AppRouter = () => {
             <Router>
                 <Routes>
                     <Route path='/' element={<App/>}/>
+                    <Route path='/index' element={<IndexPage/>}/>
                 </Routes>
             </Router>
         </>
