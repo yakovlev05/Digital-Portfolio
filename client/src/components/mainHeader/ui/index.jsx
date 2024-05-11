7ff86e7ee987aed44ab3a2ea161c755812222616
yakovlev05 <alexey.yakovlev.05@mail.ru>
2024-05-11 22:18:46 +0500

    add MainHeaderComponent

---
+++ client/src/components/mainHeader/ui/index.jsx
@@ -0,0 +1,31 @@
+import styles from './styles.module.scss'
+import logo from './img/logo.png'
+
+const MainHeaderComponent = () => {
+    return (
+        <header className={styles.header}>
+            <a className={styles.logo} href='/'>
+                <img src={logo} alt='логотип' width='96' height='96'/>
+            </a>
+            <ul className={styles.navList}>
+                <li className={styles.navElement}>
+                    <a className={styles.url} href='/'>Главная</a>
+                </li>
+                <li className={styles.navElement}>
+                    <a className={styles.url} href='/'>Рецепты</a>
+                </li>
+                <li className={styles.navElement}>
+                    <a className={styles.url} href='/'>Портфолио</a>
+                </li>
+            </ul>
+            <input className={styles.searchField} type='search' placeholder='Поиск'/>
+            <div className={styles.auth}>
+                <a className={styles.url} href={'/login'}>Вход</a>
+                /
+                <a className={styles.url} href={'/registration'}>Регистрация</a>
+            </div>
+        </header>
+    )
+}
+
+export default MainHeaderComponent;
---
+++ client/src/pages/me/ui/index.jsx
@@ -0,0 +1,8 @@
+import styles from './styles.module.scss';
+import MainHeaderComponent from "../../../components/mainHeader";
+
+const MePage = () => {
+    return (<MainHeaderComponent/>)
+}
+
+export default MePage;
