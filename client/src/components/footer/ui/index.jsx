d200e91a35376c8220ab5cb0050b0f815953e588
yakovlev05 <alexey.yakovlev.05@mail.ru>
2024-05-13 12:24:31 +0500

    add FooterComponent

---
+++ client/src/components/footer/ui/index.jsx
@@ -0,0 +1,28 @@
+import styles from './styles.module.scss';
+import logo from './img/logo.png';
+
+
+const FooterComponent = () => {
+    return (
+        <footer>
+            <div className={styles.footer}>
+                <a className={styles.photoUrl} href={"/"}>
+                    <img src={logo} alt={"logo"} width="200" height="200"/>
+                </a>
+                <ul className={styles.listNavigation}>
+                    <li className={styles.listElement}>
+                        <a className={styles.url} href={'/'}>Главная</a>
+                    </li>
+                    <li className={styles.listElement}>
+                        <a className={styles.url} href={'/'}>Рецепты</a>
+                    </li>
+                    <li className={styles.listElement}>
+                        <a className={styles.url} href={'/me'}>Портфолио</a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    )
+}
+
+export default FooterComponent;
--- client/src/pages/me/ui/index.jsx
+++ client/src/pages/me/ui/index.jsx
@@ -1,8 +1,14 @@
 import styles from './styles.module.scss';
 import MainHeaderComponent from "../../../components/mainHeader";
+import FooterComponent from "../../../components/footer";
 
 const MePage = () => {
-    return (<MainHeaderComponent imageName={"90b4a891-f05a-4134-8a63-d2c314d7fc2a.jpg"}/>)
+    return (
+        <>
+            <MainHeaderComponent imageName={"90b4a891-f05a-4134-8a63-d2c314d7fc2a.jpg"}/>
+            <FooterComponent/>
+        </>
+    )
 }
 
 export default MePage;
